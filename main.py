28963de2b9a0f32ae8c690958109bb236ada2858
exceq <mihail.petuxov@yandex.ru>
2022-05-08 19:40:20 +0500

    запускалка

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ app.include_router(user_router, prefix='/user', tags=['User'])
 @app.get('/')
 async def hello():
     return {'message': 'Hello world!'}
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
+    uvicorn.run('main:app', host='0.0.0.0', port=8080, reload=True)
