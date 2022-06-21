cc147a9c8eac0241a9103562c3e9a6e67bc2fc3c
exceq <mihail.petuxov@yandex.ru>
2022-06-22 01:15:22 +0500

    поиск хештегов в тексте

--- postprocessor/domain/parsing.py
+++ postprocessor/domain/parsing.py
@@ -17,10 +17,25 @@ def get_links(text: str):
     return result
 
 
+def get_hashtags(text):
+    """Находит хештеги в тексте"""
+    pattern = r'(#[a-zA-Zа-яА-Я]{0,30})'
+    links = re.findall(pattern, text)
+
+    result = []
+    for link in links:
+        result.append(
+            Extra(text=link, offset=text.find(link), length=len(link))
+        )
+
+    return result
+
+
 def get_extra(text: str):
     """Находит в тексте экстра-данные: ссылки, упоминания и т.д."""
     extra = []
 
     extra += get_links(text)
+    extra += get_hashtags(text)
 
     return extra
